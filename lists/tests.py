2eccebeecf13b7ee971bef578c4754a50f2893f9
jplc <lcjuanpa@gmail.com>
2017-09-08 16:46:15 -0400

    Refactor home page view to use a template

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,8 @@
+from lists.views import homePage
 from django.http import HttpRequest
 from django.test import TestCase
 from django.core.urlresolvers import resolve
-from lists.views import homePage
+from django.template.loader import render_to_string
 
 class HomePageTest(TestCase):
 
@@ -12,6 +13,16 @@ class HomePageTest(TestCase):
   def testHomePageReturnsCorrectHtml(self):
     request = HttpRequest()
     response = homePage(request)
-    self.assertTrue(response.content.startswith(b'<html>'))
-    self.assertIn(b'<title>To-Do lists</title>', response.content)
-    self.assertTrue(response.content.endswith(b'</html>'))
+    # self.assertTrue(response.content.strip().startswith(b'<!DOCTYPE html>')) # strip remove empty lines
+    # self.assertIn(b'<title>To-Do lists</title>', response.content)
+    # self.assertTrue(response.content.strip().endswith(b'</html>')) # strip remove empty lines
+    # b' -> means bytes
+
+    # En lugar de testiar bytes con texto constante, ahora se verifica la
+    # logica (con ayuda de una comparacion de cadenas de los contenidos).
+    ## One of the rules of unit testing is Don’t test constants, and testing
+    ## HTML as text is a lot like testing a constant.
+    ## Never do "wibble = 3 | from myprogram import wibble | assert wibble == 3"
+    ## Unit tests are really about testing logic, flow control, and configuration.
+    expectedHtml = render_to_string('lists/home.html')
+    self.assertEqual(response.content.decode(), expectedHtml)
--- lists/views.py
+++ lists/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def homePage(request):
-  return HttpResponse('<html><title>To-Do lists</title></html>')
+  return render(request, 'lists/home.html')
 
